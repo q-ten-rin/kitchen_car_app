f02b90a633fe8f8f6efe685e4a0c6fe86cd8b7ec
菅原凜 <sugawararin@MacBook-Air.local>
2025-11-09 17:11:46 +0900

    ログイン・ログアウト機能の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    validates :name, presence: true
+    validates :name, presence: true 
     validates :email, presence: true, uniqueness: true
     
     has_many :posts, dependent: :destroy
