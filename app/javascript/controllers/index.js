e6a858bd8acbd0d56ea1786849a9bcb941701993
菅原凜 <sugawararin@MacBook-Air.local>
2025-11-12 16:14:47 +0900

    Fix: デプロイでエラーが起きたので修正2

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,4 +1,7 @@
-// Import and register all your controllers from the importmap via controllers/**/*_controller
-import { application } from "controllers/application"
-import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-eagerLoadControllersFrom("controllers", application)
+import { application } from "./application"
+
+// controllers フォルダ内のコントローラを自動読み込み
+import { eagerLoadControllersFrom } from "@hotwired/stimulus"
+
+// フォルダ内のコントローラをまとめてロード
+eagerLoadControllersFrom("./controllers", application)
