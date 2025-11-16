b35f5eee89097349b4df6a500464441366324927
菅原凜 <sugawararin@MacBook-Air.local>
2025-11-16 23:01:47 +0900

    投稿削除機能の追加

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,11 +1,1 @@
 import { application } from "./application"
-
-const controllers = import.meta.glob("./*_controller.js", { eager: true })
-
-for (const path in controllers) {
-  const name = path
-    .split("/")
-    .pop()
-    .replace("_controller.js", "")
-  application.register(name, controllers[path].default)
-}
