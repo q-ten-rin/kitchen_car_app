9c72fec003172f3097801c5418bc39b90c3f37ab
菅原凜 <sugawararin@MacBook-Air.local>
2025-11-20 23:27:37 +0900

    fix: CSSの修正

--- db/schema.rb
+++ db/schema.rb
@@ -72,4 +72,14 @@ ActiveRecord::Schema[7.2].define(version: 2025_11_18_100710) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+
+ActiveRecord::Schema[7.2].define(version: 2025_10_29_072553) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 end
